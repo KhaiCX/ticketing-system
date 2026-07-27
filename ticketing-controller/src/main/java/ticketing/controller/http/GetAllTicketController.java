406e91136c50e1d522803b87c6de938a5e71054c
KhaiCX <khaicx2001@gmail.com>
2026-07-28 01:20:30 +0700

    khaicx: integrates Prometheus and Grafana

--- ticketing-controller/src/main/java/ticketing/controller/http/GetAllTicketController.java
+++ ticketing-controller/src/main/java/ticketing/controller/http/GetAllTicketController.java
@@ -18,17 +18,19 @@ import java.util.List;
 public class GetAllTicketController {
     private final GetAllTicketApplicationService eventAppService;
     private final RestTemplate restTemplate;
+
     @GetMapping
     @RateLimiter(name = "backendA", fallbackMethod = "fallbackHello")
     public ResponseEntity<List<TicketDomain>> getAllTickets() {
         return ResponseEntity.ok(eventAppService.getAll());
     }
 
-    public String fallbackHello(Throwable throwable) {
-        return "Too many request";
+    public ResponseEntity<?> fallbackHello(Throwable throwable) {
+        return ResponseEntity.status(429).body("Too many request");
     }
-    public String fallbackCircuit(Throwable throwable) {
-        return "fallbackCircuit";
+
+    public ResponseEntity<?> fallbackCircuit(Throwable throwable) {
+        return ResponseEntity.status(429).body("Too many request");
     }
 
     @GetMapping("/hi/v1")
@@ -36,7 +38,7 @@ public class GetAllTicketController {
     public String hiV1() {
         String url = "https://fakestoreapi.com/products/1";
         return restTemplate.getForObject(url, String.class);
-        //return eventAppService.sayHi("KhaiCX v1");
+        // return eventAppService.sayHi("KhaiCX v1");
     }
 
 }
